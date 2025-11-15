9b1e9d1882f7448059f8eee0d73045815f8cbe80
Juan Pablo <jusanchezvi@unal.edu.co>
2025-11-15 12:15:28 -0500

    Done!

--- homework/src/_internals/preprocess_lines.py
+++
@@ -1,6 +0,0 @@
-from typing import Iterable, List
-
-
-def preprocess_lines(lines: Iterable[str]) -> List[str]:
-    """Normalize whitespace and lowercase each line."""
-    return [line.strip().lower() for line in lines if line.strip()]
